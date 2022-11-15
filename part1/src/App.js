4193edaf5dc5eccb9f27d0b9a31fe67466a6935e
Henri <henri.mykra@esedulainen.fi>
2022-11-15 09:25:30 +0200

    part1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,18 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+    </div>
+  )
+}
 
 export default App
