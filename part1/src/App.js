b65e297c5317f18ab4d0b0cdb2fbee100f60b5b6
Henri <henri.mykra@esedulainen.fi>
2022-11-15 10:25:44 +0200

    part1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,16 +1,42 @@
-const Hello = () => {
+const Header = ({course}) => {
   return (
     <div>
-      <p>Hello world</p>
+      <h1>{course}</h1>
     </div>
   )
 }
+const Content = () => {
 
-const App = () => {
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello />
+      <p>
+        
+      </p>
+    </div>
+  )
+}
+// const Total = ({exercises1, exercises2, exercises3}) => {
+//   return (
+//     <div>
+//       <p> Number of exercises {exercises1 + exercises2 + exercises3}</p>
+//     </div>
+//   )
+// }
+const App = () => {
+  // const-määrittelyt
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      {/* <Total /> */}
     </div>
   )
 }
