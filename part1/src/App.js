d20a544058ff0057a6cd47875fada533d4c39842
Alsprite <henri.mykra@esedulainen.fi>
2022-11-16 10:10:31 +0200

    1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,7 +3,7 @@
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </div>
   )
 }
@@ -29,28 +29,31 @@ const Total = (props) => {
   )
 }
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-    name: 'Fundamentals of React',
-    exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-    return (
-      <div>
-        <Header course = {course} />
-        <Content p = {parts} />
-        <Total p = {parts} />
-      </div>
-    )
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
+
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content p = {course.parts} />
+      <Total p = {course.parts} />
+    </div>
+  )
+}
 
 export default App
