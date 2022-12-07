ee407ab64bd92ea1b4d567fbeb0d52d0c5067338
Alsprite <henri.mykra@esedulainen.fi>
2022-12-07 13:20:46 +0200

    fullstack package cyclic homma

--- hiekkalaatikko/src/index.js
+++ hiekkalaatikko/src/index.js
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
+import express from 'express'
 import axios from 'axios'
+import { application } from 'express'
+
+app.use(express.static('build'))
 
 axios.get('http://localhost:3001/notes').then(response => {
   const notes = response.data
