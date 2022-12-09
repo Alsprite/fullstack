49ef8f11db891ce996cdcf3be06ef1c6d6f25f8e
Henri <henri.mykra@esedulainen.fi>
2022-12-09 11:14:35 +0200

    aaa

--- hiekkalaatikko/src/services/notes.js
+++ hiekkalaatikko/src/services/notes.js
@@ -1,12 +1,10 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
-
   return request.then(response => response.data)
 }
-
 const create = newObject => {
   const request = axios.post(baseUrl, newObject)
   return request.then(response => response.data)
