6982c3f1be721e36e81b6f1716472913edba2a00
Henri <henri.mykra@esedulainen.fi>
2023-01-13 10:55:16 +0200

    4.5

---
+++ part4.1/utils/list_helper.js
@@ -0,0 +1,14 @@
+const dummy = (blogs) => {
+    return 1
+}
+const totalLikes = (blogs) => {
+    return blogs.reduce((total, blog) => total + blog.likes, 0)
+}
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((a, b) => (a.likes > b.likes ? a : b))
+}
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog
+}
