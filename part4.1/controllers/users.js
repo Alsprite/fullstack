9a1cf833cf7a1f20d01c8bbf8f2c8a5aafad147c
Alsprite <henri.mykra@esedulainen.fi>
2023-01-23 09:30:06 +0200

    4.15

--- part4.1/controllers/users.js
+++ part4.1/controllers/users.js
@@ -2,6 +2,24 @@ const bcrypt = require('bcryptjs')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', (request, response) => {
+  User.find({}).then(users => {
+    response.json(users)
+  })
+})
+
+usersRouter.get('/:id', (request, response, next) => {
+  User.findById(request.params.id)
+    .then(user => {
+      if (user) {
+        response.json(user)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
