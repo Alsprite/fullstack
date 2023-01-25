fb01fd6d10e9e5fd36b3aef4d975452e54f73a8c
Henri <henri.mykra@esedulainen.fi>
2023-01-25 09:00:11 +0200

    a

--- part4.1/models/blog.js
+++ part4.1/models/blog.js
@@ -5,6 +5,10 @@ const blogSchema = mongoose.Schema({
   author: String,
   url: String,
   likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 blogSchema.set('toJSON', {
