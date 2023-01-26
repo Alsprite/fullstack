f6b7540dfc5e612dcae2ede74ef8a1cfc4486b98
Alsprite <henri.mykra@esedulainen.fi>
2023-01-26 12:31:20 +0200

    5.4.

--- part5.1/src/App.js
+++ part5.1/src/App.js
@@ -3,10 +3,12 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
   const alreadyUser = window.localStorage.getItem('loggedUser')
@@ -16,18 +18,30 @@ const App = () => {
       blogService.getAll().then(blogs => setBlogs(blogs))
     }
   }, [])
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMessage(null);
+    }, 5000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [message]);
 
   const handleLogin = async event => {
     event.preventDefault()
-    let uid = document.forms["log"]["nimi"].value;
-    let pas = document.forms["log"]["pass"].value;
-    const user = await loginService.login({
+    try {
+      let uid = document.forms["log"]["nimi"].value;
+      let pas = document.forms["log"]["pass"].value;
+      const user = await loginService.login({
       username: uid,
       password: pas
     })
     blogService.setToken(user.token)
     setUser(user)
     window.localStorage.setItem('loggedUser', JSON.stringify(user))
+    } catch (exception) {
+      setMessage("error" + exception.response.data.error);
+    }
   }
   const handleLogOut = async event => {
     event.preventDefault()
@@ -36,33 +50,41 @@ const App = () => {
   }
   const newBlog = async event => {
     event.preventDefault()
-    const newB = {
-      user: user,
-      title: document.forms["blog"]["title"].value,
-      author: document.forms["blog"]["author"].value,
-      url: document.forms["blog"]["url"].value
-    }
-    const auth = user.token
-      blogService.create(newB, auth).then(blog => {
+    try {
+        const newB = {
+        user: user,
+        title: document.forms["blog"]["title"].value,
+        author: document.forms["blog"]["author"].value,
+        url: document.forms["blog"]["url"].value
+      }
+        const auth = user.token
+        blogService.create(newB, auth).then(blog => {
         setBlogs(blogs.concat(blog))
+        setMessage(`A new blog ${newB.title} by ${newB.author} added`)
     })
+    } catch (exception) {
+      setMessage("error" + exception.response.data.error);
+    }
   }
   if (user === null) {
     return (
       <div>
         <h2>Log in to application</h2>
+        <Notification message={message}></Notification>
         <form name="log" onSubmit={handleLogin}>
           <p>Username</p>
-          <input type="text" name="nimi" onChange={handleLogin} ></input>
+          <input type="text" name="nimi" onChange={console.log("jee")} ></input>
           <p>Password</p>
-          <input type="password" name="pass" onChange={handleLogin} ></input>
+          <input type="password" name="pass" onChange={console.log("joo")} ></input>
         </form>
+        <button type="submit" onClick={handleLogin}>Log in</button>
       </div>
     )
   } else {
     return (
     <div>
       <h2>blogs</h2>
+      <Notification message={message} />
       <p>{user.username} logged in</p>
       <button onClick={handleLogOut}>Log out</button>
       <h2>create new</h2>
