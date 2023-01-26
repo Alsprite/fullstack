4533cc02eb1fbb4bbd938c71db4bebe77dc7d4d8
Alsprite <henri.mykra@esedulainen.fi>
2023-01-26 11:29:58 +0200

    5.1.

--- part5.1/src/App.js
+++ part5.1/src/App.js
@@ -1,24 +1,55 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    )
   }, [])
 
-  return (
+  const handleLogin = async event => {
+    event.preventDefault()
+    let uid = document.forms["log"]["nimi"].value;
+    let pas = document.forms["log"]["pass"].value;
+    const user = await loginService.login({
+      username: uid,
+      password: pas
+    })
+    blogService.setToken(user.token)
+    setUser(user)
+  }
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form name="log" onSubmit={handleLogin}>
+          <p>Username</p>
+          <input type="text" name="nimi" onChange={handleLogin} ></input>
+          <p>Password</p>
+          <input type="password" name="pass" onChange={handleLogin} ></input>
+        </form>
+      </div>
+    )
+  } else {
+    return (
     <div>
       <h2>blogs</h2>
+      <p>{user.username} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
   )
+  }
+
+  
 }
 
 export default App
