e68529612bd19f1fd22846bc4076071799fcfd1f
Alsprite <henri.mykra@esedulainen.fi>
2023-02-08 13:04:19 +0200

    sisennyst

--- part5.1/src/components/BlogForm.test.js
+++ part5.1/src/components/BlogForm.test.js
@@ -1,27 +1,25 @@
-import React from "react";
-import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
-import BlogForm from "./BlogForm";
-import userEvent from "@testing-library/user-event";
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import BlogForm from './BlogForm'
+import userEvent from '@testing-library/user-event'
 
-test("<BlogForm /> updates parent state and calls onSubmit", async () => {
-  const createBlog = jest.fn();
-  const user = userEvent.setup();
+test('<BlogForm /> updates parent state and calls onSubmit', async () => {
+  const createBlog = jest.fn()
+  const user = userEvent.setup()
+  const component = render(<BlogForm addBlog={createBlog} />)
+  const titleInput = component.container.querySelector('input[name=\'title\']')
+  const authorInput = component.container.querySelector('input[name=\'author\']')
+  const urlInput = component.container.querySelector('input[name=\'url\']')
+  const sendButton = screen.getByText('Create')
 
-  const container = render(<BlogForm createBlog={createBlog} />);
+  await user.type(titleInput, 'title')
+  await user.type(authorInput, 'author')
+  await user.type(urlInput, 'https://www.test.com')
+  await user.click(sendButton)
 
-  const titleInput = container.querySelector("input[name='title']");
-  const authorInput = container.querySelector("input[name='author']");
-  const urlInput = container.querySelector("input[name='url']");
-  const sendButton = screen.getByText("Create");
-
-  await user.type(titleInput, "title");
-  await user.type(authorInput, "author");
-  await user.type(urlInput, "https://www.test.com");
-  await user.click(sendButton);
-
-  expect(createBlog.mock.calls).toHaveLength(1);
-  expect(createBlog.mock.calls[0][0]).toBe("title");
-  expect(createBlog.mock.calls[0][1]).toBe("author");
-  expect(createBlog.mock.calls[0][2]).toBe("https://www.test.com");
-});
+  expect(createBlog.mock.calls).toHaveLength(1)
+  expect(createBlog.mock.calls[0][0].content).toBe('title')
+  expect(createBlog.mock.calls[0][1].content).toBe('author')
+  expect(createBlog.mock.calls[0][2].content).toBe('https://www.test.com')
+})
