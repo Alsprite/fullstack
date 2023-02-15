4b5e3a9062177959d85ac528b9528d30554e6ad3
Alsprite <henri.mykra@esedulainen.fi>
2023-02-15 12:32:55 +0200

    kommentointi

--- part4.1/controllers/testing.js
+++ part4.1/controllers/testing.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
+// const User = require('../models/user')
 
 router.post('/reset', async (request, response) => {
   await Blog.deleteMany({})
-  await User.deleteMany({})
+  // await User.deleteMany({})
 
   response.status(204).end()
 })
