11fbb22ded4cb82fae7be3380ff99e9b0594cf57
Alsprite <henri.mykra@esedulainen.fi>
2023-02-23 10:01:46 +0200

    6.10.

--- part6.3/src/reducers/filterReducer.js
+++ part6.3/src/reducers/filterReducer.js
@@ -1,18 +1,17 @@
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
-      return action.filter;
-    default:
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = "";
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    setFilter(state, action) {
+      state = action.payload;
       return state;
+    }
   }
-};
+})
 
-const setFilter = (filter) => {
-  return {
-    type: 'SET_FILTER',
-    filter,
-  };
-};
-
-export { setFilter };
-export default filterReducer;
+export const { setFilter } = filterSlice.actions;
+export default filterSlice.reducer;
