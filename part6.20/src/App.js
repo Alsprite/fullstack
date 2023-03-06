9fb37edf28c3c60b2dd40412634cd0bd8a0502c1
Alsprite <henri.mykra@esedulainen.fi>
2023-03-06 12:12:47 +0200

    6.22

--- part6.20/src/App.js
+++ part6.20/src/App.js
@@ -1,8 +1,22 @@
-import { useQuery } from 'react-query'
-import axios from 'axios'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { getAnecdotes, createAnecdote, updateAnecdote } from './requests'
 
 const App = () => {
-  const result = useQuery('anecdotes', () => axios.get('http://localhost:3001/anecdotes').then(res => res.data))
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes')
+    }
+  })
+
+  const updatedAnecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes')
+    }
+  })
+
+  const result = useQuery('anecdotes', getAnecdotes)
   console.log(result)
 
   if (result.isLoading) {
@@ -14,13 +28,33 @@ const App = () => {
 
   const anecdotes = result.data
 
+  const addAnecdote = async (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({content})
+  }
+  const voteAnecdote = async (id, votes) => {
+    const updatedAnecdote = { ...anecdotes.find(a => a.id === id), votes: votes + 1 };
+    await updatedAnecdoteMutation.mutateAsync(updatedAnecdote);
+    queryClient.invalidateQueries('anecdotes');
+  };
+
   return (
     <div>
       <h2>Anecdotes</h2>
+      <form onSubmit={addAnecdote}>
+        <input name="anecdote" />
+        <button type="submit">Create</button>
+      </form>
+      <br></br>
       {anecdotes.slice().map(anecdote => 
         <div key={anecdote.id}>
           <div>
+            <br></br>
             {anecdote.content}
+            <p>Votes: {anecdote.votes}</p>
+            <button onClick={() => voteAnecdote(anecdote.id, anecdote.votes)}>Vote</button>
           </div>
           </div>
         )}
