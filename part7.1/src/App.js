fa552e1ae49d80610c3ae73874deb4dec6c4721f
Alsprite <henri.mykra@esedulainen.fi>
2023-03-09 10:45:14 +0200

    7.6

--- part7.1/src/App.js
+++ part7.1/src/App.js
@@ -85,15 +85,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content} />
+          <input type={content.type} value={content.value} onChange={content.onChange} />
         </div>
         <div>
           author
-          <input {...author} />
+          <input type={author.type} value={author.value} onChange={author.onChange} />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input type={info.type} value={info.value} onChange={info.onChange} />
         </div>
         <button>create</button>
         <button type="reset" onClick={handleReset}>reset</button>
