91d856a3b16b821f45485319b4dec5e54ebd011f
Alsprite <henri.mykra@esedulainen.fi>
2023-03-10 10:03:07 +0200

    7.8

--- part7.8/src/App.js
+++ part7.8/src/App.js
@@ -1,37 +1,5 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-
-const useField = (type) => {
-  const [value, setValue] = useState('')
-
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  return {
-    type,
-    value,
-    onChange
-  }
-}
-
-const useResource = (baseUrl) => {
-  const [resources, setResources] = useState([])
-
-  // ...
-
-  const create = (resource) => {
-    // ...
-  }
-
-  const service = {
-    create
-  }
-
-  return [
-    resources, service
-  ]
-}
+import { useField, useResource } from "./hooks";
+import { useEffect } from "react";
 
 const App = () => {
   const content = useField('text')
@@ -50,7 +18,6 @@ const App = () => {
     event.preventDefault()
     personService.create({ name: name.value, number: number.value})
   }
-
   return (
     <div>
       <h2>notes</h2>
@@ -58,6 +25,7 @@ const App = () => {
         <input {...content} />
         <button>create</button>
       </form>
+
       {notes.map(n => <p key={n.id}>{n.content}</p>)}
 
       <h2>persons</h2>
@@ -66,7 +34,9 @@ const App = () => {
         number <input {...number} />
         <button>create</button>
       </form>
+
       {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+
     </div>
   )
 }
