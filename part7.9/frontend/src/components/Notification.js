9c99372aaa8ed8d41fdd0d514dbd282fa6bbbbd0
Alsprite <henri.mykra@esedulainen.fi>
2023-03-14 12:01:29 +0200

    7.14 alku

--- part7.9/frontend/src/components/Notification.js
+++
@@ -1,8 +0,0 @@
-const Notification = ({ message }) => {
-  if (message === null) return null;
-  if (message.includes("error")) {
-    return <div className="error">{message.substring(5)}</div>;
-  }
-  return <div className="success">{message}</div>;
-};
-export default Notification;
