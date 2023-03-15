ec05923e34a2801d897b41ba1c65fa8fcf4eb20b
Alsprite <henri.mykra@esedulainen.fi>
2023-03-15 10:11:11 +0200

    7.14. tavallaan

--- part7.9/frontend/src/components/Users.js
+++ part7.9/frontend/src/components/Users.js
@@ -1,14 +1,43 @@
 const User = (props) => {
-    console.log(props.blogs)
-    return (
-        <div>
-        <h1>Users</h1>
-        <ul>
-        {props.blogs.map((user, index) => (
-          <li key={index}>{user)}</li>
-        ))}
-      </ul>
-        </div>
-    )
+
+  const blogsByUser = props.blogs.reduce((acc, blog) => {
+    const user = blog.user.username;
+    if (!acc[user]) {
+      acc[user] = [];
+    }
+    acc[user].push(blog);
+    return acc;
+  }, {});
+
+  const users = Object.keys(blogsByUser);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>User</th>
+            <th>Blogs</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => 
+            <tr key={user}>
+              <td>{user}</td>
+              <td>
+                <ul>
+                  {blogsByUser[user].map(blog => 
+                    <li key={blog.id}>{blog.title}</li>
+                  )}
+                </ul>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
 }
+
 export default User
