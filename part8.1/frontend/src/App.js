ef2027011f2cc1a721b51bf5d994b9f67fb3e82f
Alsprite <henri.mykra@esedulainen.fi>
2023-03-23 10:08:21 +0200

    8.10

--- part8.1/frontend/src/App.js
+++ part8.1/frontend/src/App.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom'
 import Authors from './components/authors'
 import Books from './components/books'
+import Addbook from './components/addbook'
 
 const ALL_AUTHORS = gql`
   query {
@@ -30,11 +31,13 @@ const App = () => {
       <Link style={padding} to="/"></Link>
       <Link style={padding} to="/authors">Authors</Link>
       <Link style={padding} to="/books">Books</Link>
+      <Link style={padding} to="/addBook">Add book</Link>
     </div>
 
     <Routes>
       <Route path="/authors" element={<Authors loading={loading} data={data}/>} />
       <Route path="/books" element={<Books />} />
+      <Route path="/addBook" element={<Addbook />} />
     </Routes>
     </Router>
   )
---
+++ part8.1/frontend/src/components/addbook.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import { gql, useMutation } from '@apollo/client'
+
+const Add = (props) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [published, setPublished] = useState('')
+  const [genre, setGenre] = useState('')
+
+  const ADD_BOOK = gql`
+  mutation addBook($title: String!, $author: String!, $published: Int! , $genres: [String!]!) {
+    addBook(
+      title: $title,
+      author: $author,
+      published: $published,
+      genres: $genres
+    ) {
+      title
+      author {
+        name
+      }
+      published
+      genres
+    }
+  }
+`
+
+  const [createBook] = useMutation(ADD_BOOK)
+
+  const handleNewBook = (event) => {
+    event.preventDefault()
+    createBook({ variables: { title, author, published: parseInt(published), genres: [genre] } })
+
+    setTitle('')
+    setAuthor('')
+    setPublished('')
+    setGenre('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleNewBook}>
+        <input placeholder="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <br></br>
+        <input placeholder="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+        <br></br>
+        <input placeholder="published" type="number" value={published} onChange={(e) => setPublished(e.target.value)} />
+        <br></br>
+        <input placeholder="genre" value={genre} onChange={(e) => setGenre(e.target.value)} />
+        <button type="submit">add genre</button>
+        <br></br>
+        <button type="submit">create book</button>
+      </form>
+    </div>
+  )
+}
+
+export default Add
