5adb8107dd21be757e41ee3cc2d624eb70d1a042
Alsprite <henri.mykra@esedulainen.fi>
2023-04-17 09:37:05 +0300

    9.22

--- part9.8/frontend/src/types.ts
+++ part9.8/frontend/src/types.ts
@@ -10,6 +10,45 @@ export enum Gender {
   Other = "other"
 }
 
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry = | HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
+
 export interface Patient {
   id: string;
   name: string;
