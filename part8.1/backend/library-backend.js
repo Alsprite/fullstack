f4a9fe29e1e7b4094078b265dfaabae988f1c66a
Alsprite <henri.mykra@esedulainen.fi>
2023-09-25 11:55:26 +0300

    8.13. täysin alusta

--- part8.1/backend/library-backend.js
+++ part8.1/backend/library-backend.js
@@ -1,22 +1,88 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', false)
-const Author = require('./models/Author')
-const Book = require('./models/book')
+const { v4: uuidv4 } = require('uuid')
 
-require('dotenv').config()
-const { MONGODB_URI } = require('./config')
+let authors = [
+  {
+    name: 'Robert Martin',
+    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    born: 1952,
+  },
+  {
+    name: 'Martin Fowler',
+    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    born: 1963
+  },
+  {
+    name: 'Fyodor Dostoevsky',
+    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    born: 1821
+  },
+  { 
+    name: 'Joshua Kerievsky', // birthyear not known
+    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+  },
+  { 
+    name: 'Sandi Metz', // birthyear not known
+    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+  },
+]
 
-console.log('connecting to', MONGODB_URI)
+let books = [
+  {
+    title: 'Clean Code',
+    published: 2008,
+    author: 'Robert Martin',
+    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring']
+  },
+  {
+    title: 'Agile software development',
+    published: 2002,
+    author: 'Robert Martin',
+    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    genres: ['agile', 'patterns', 'design']
+  },
+  {
+    title: 'Refactoring, edition 2',
+    published: 2018,
+    author: 'Martin Fowler',
+    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring']
+  },
+  {
+    title: 'Refactoring to patterns',
+    published: 2008,
+    author: 'Joshua Kerievsky',
+    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring', 'patterns']
+  },  
+  {
+    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
+    published: 2012,
+    author: 'Sandi Metz',
+    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring', 'design']
+  },
+  {
+    title: 'Crime and punishment',
+    published: 1866,
+    author: 'Fyodor Dostoevsky',
+    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    genres: ['classic', 'crime']
+  },
+  {
+    title: 'The Demon ',
+    published: 1872,
+    author: 'Fyodor Dostoevsky',
+    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    genres: ['classic', 'revolution']
+  },
+]
 
-mongoose.connect(MONGODB_URI)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
+/*
+  you can remove the placeholder query once your first own has been implemented 
+*/
 
 const typeDefs = `
 type Author {
@@ -54,25 +120,20 @@ type Query {
 
 const resolvers = {
   Query: {
-    booksCount: () => Book.collection.countDocuments(),
-    authorCount: () => Author.collection.countDocuments(),
-    allBooks: async (parent, args) => {
-      let books = await Book.find().populate('author')
-      // console.log(books[0])
+    booksCount: () => books.length,
+    authorCount: () => authors.length,
+    allBooks: (parent, args) => {
+      console.log(args)
       if (args.author) {
-        console.log(args.author)
-        console.log(books[0].Author)
-        books = books.filter(book => book.author === args.author)
-      }
-      if (args.genre) {
-        books = books.find({ genres: args.genre })
+        return books.filter(book => book.author === args.author)
       }
 
+      if (args.genre) {
+        return books.filter(book => book.genres.includes(args.genre))
+      }
       return books
     },
-    allAuthors: async () => {
-      let books = await Book.find()
-      let authors = await Author.find()
+    allAuthors: () => {
       const authorsWithBooks = authors.map(author => ({
         name: author.name,
         born: author.born,
@@ -85,35 +146,34 @@ const resolvers = {
   },
   Book: {
     author: (root) => {
-      // console.log(root)
-      return root
+      return authors.find(a => a.name === root.author)
     }
   },
-  // Mutation: {
-  //   addBook: (parent, args) => {
-  //     const { title, author, published, genres} = args
-  //     const authorExists = authors.find(a => a.name === author)
+  Mutation: {
+    addBook: (parent, args) => {
+      const { title, author, published, genres} = args
+      const authorExists = authors.find(a => a.name === author)
     
-  //     if (!authorExists) {
-  //       const newAuthor = { name: author, id: uuidv4() }
-  //       authors = authors.concat(newAuthor)
-  //     }
+      if (!authorExists) {
+        const newAuthor = { name: author, id: uuidv4() }
+        authors = authors.concat(newAuthor)
+      }
 
-  //     const newBook = {...args, id: uuidv4()}
-  //     books = books.concat(newBook)
-  //     return newBook
-  //     },
-  //     editAuthor: (parent, args) => {
-  //       const { name, born } = args;
-  //       const author = authors.find(author => author.name === name);
-  //       if (!author) {
-  //         return null;
-  //       }
-  //       const updatedAuthor = { ...author, born };
-  //       authors = authors.map(a => a.id === author.id ? updatedAuthor : a);
-  //       return updatedAuthor;
-  //     }
-  //   }
+      const newBook = {...args, id: uuidv4()}
+      books = books.concat(newBook)
+      return newBook
+      },
+      editAuthor: (parent, args) => {
+        const { name, born } = args;
+        const author = authors.find(author => author.name === name);
+        if (!author) {
+          return null;
+        }
+        const updatedAuthor = { ...author, born };
+        authors = authors.map(a => a.id === author.id ? updatedAuthor : a);
+        return updatedAuthor;
+      }
+    }
 }
 
 const server = new ApolloServer({
