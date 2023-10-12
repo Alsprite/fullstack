d116c4f4037cdbe7b444aac6f6c3b0a6f76a9de2
Alsprite <henri.mykra@esedulainen.fi>
2023-10-12 12:45:13 +0300

    8.21. toimii

--- part8.1/frontend/src/components/books.js
+++ part8.1/frontend/src/components/books.js
@@ -5,66 +5,70 @@ const Books = (props) => {
 const [genresToShow, setGenresToShow] = useState(null)
 
 const ALL_BOOKS = gql`
-    query {
-      allBooks {
-        title
-        author {
-          name
-          born
-          bookCount
-        }
-        published
-        genres
-        id
+  query Query($genre: String) {
+    allBooks(genre: $genre) {
+      title
+      author {
+        name
+        born
+        bookCount
       }
+      published
+      genres
+      id
+    }
   }
 `
-    const { loading, data } = useQuery(ALL_BOOKS)
 
-    if (loading) {
-        return <div>loading...</div>
-    }
-    const handleGenreChange = (event) => {
-      setGenresToShow(event.target.value)
-    }
-  
-    const filteredBooks = genresToShow
-      ? data.allBooks.filter(book => book.genres.includes(genresToShow))
-      : data.allBooks
+const { loading, data } = useQuery(ALL_BOOKS, {
+  variables: { genre: genresToShow },
+})
 
-    return (
-      <div>
-      <h1>Books</h1>
-      <p>Filter by genre: {genresToShow}</p>
-      <select onChange={handleGenreChange}>
-        <option value="">All Genres</option>
-        {data.allBooks
-          .flatMap(book => book.genres)
-          .filter((genre, index, self) => self.indexOf(genre) === index)
-          .map(genre => (
-            <option key={genre} value={genre}>
-              {genre}
-            </option>
-          ))}
-      </select>
-      <table>
-        <tbody>
-          <tr>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Published</th>
-          </tr>
-          {filteredBooks.map(b => (
-            <tr key={b.id}>
-              <td>{b.title}</td>
-              <td>{b.author.name}</td>
-              <td>{b.published}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-    )
+if (loading) {
+  return <div>loading...</div>
 }
 
-export default Books
+const handleGenreChange = (event) => {
+  setGenresToShow(event.target.value)
+}
+
+const filteredBooks = genresToShow
+  ? data.allBooks.filter((book) => book.genres.includes(genresToShow))
+  : data.allBooks
+
+return (
+  <div>
+    <h1>Books</h1>
+    <p>Filter by genre: {genresToShow}</p>
+    <select onChange={handleGenreChange}>
+      <option value="">All Genres</option>
+      {data.allBooks
+        .flatMap((book) => book.genres)
+        .filter((genre, index, self) => self.indexOf(genre) === index)
+        .map((genre) => (
+          <option key={genre} value={genre}>
+            {genre}
+          </option>
+        ))}
+    </select>
+    <table>
+      <tbody>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Published</th>
+        </tr>
+        {filteredBooks.map((b) => (
+          <tr key={b.id}>
+            <td>{b.title}</td>
+            <td>{b.author.name}</td>
+            <td>{b.published}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
+};
+
+export default Books;
